7a7ad2565b1d9fca4ca634e62b538d976f1d2c15
Daniel Nhemihes <danielnhemihes@live.com>
2023-03-26 19:23:17 -0300

    FEAT: NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app-container">
+    <div class="side-nav">
+      <NavBar/>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
+    
+  </div>
 </template>
 
+
+<script>
+  import NavBar from '@/components/NavBar.vue'
+
+  export default {
+  name: 'App',
+  components: {
+    NavBar
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
+  html, body {
+    margin: 0;
+    height: 100%;
+  }
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
 
-nav {
-  padding: 30px;
-}
+  .app-container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    background: linear-gradient(183.42deg, #235E65 4.92%, #0F282B 98.39%);
+  }
+  .side-nav {
+    width: 100px;
+    height: 100%;
+  }
+  .content {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+    background: #EEEEEE;
+    border-radius: 30px 0px 0px 30px;
+  }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="nav-bar">
+        <router-link to="/#" class="nav-logo">
+            <i class="bi bi-heart-fill"></i>
+        </router-link>
+        <div class="nav-select">
+            <router-link to="/#" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-house-fill"></i>
+            </router-link>
+
+            <router-link to="/about" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-clipboard-fill"></i>
+            </router-link>
+
+            <router-link to="/about" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-activity"></i>
+            </router-link>
+
+            <router-link to="/about" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-person-fill"></i>
+            </router-link>
+
+            <router-link to="/about" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-gear-fill"></i>
+            </router-link>
+        </div>
+        <div class="nav-end">
+            <router-link to="/about" active-class="nav-btn-active" class="nav-btn">
+                <i class="bi bi-box-arrow-in-left" style="font-size: 25px;"></i>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script >
+export default {
+  name: 'NavBar',
+
+}
+</script>
+
+<style scoped>
+.nav-bar{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+}
+.nav-logo{
+    color: white;
+    margin-top: 30px;
+    font-size: 40px;
+    flex-grow: 1;
+}
+.nav-select{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    align-items: center;
+}
+.nav-end{
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    align-items: center;
+}
+
+.nav-btn{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    height: 50px;
+    width: 50px;
+    margin: 10px;
+    color: white;
+    font-size: 20px;
+
+    cursor: pointer;
+    transition: 300ms;
+}
+.nav-btn-active{
+    margin-left: 40px;
+    width: 90px;
+    padding-right: 30px;
+    background-color: #EEEEEE;
+    color: #235E65;
+}
+.nav-btn:hover{
+    background-color:#235E65;
+    color: white;
+}
+</style>
