419e31a8e0f13170297bc238bf63d718900978a7
Daniel Nhemihes <danielnhemihes@live.com>
2023-06-14 10:35:43 -0300

    FEAT: ProfileTab views

---
+++ frontend/src/services/profile.js
@@ -0,0 +1,70 @@
+import axios from 'axios'
+const API_URL = `http://127.0.0.1:8000/api`;
+
+class ProfileTabService {
+    async getInfo(username) {
+        try{
+            const response = await axios.get(`${API_URL}/info_person/${username}/`, {
+                username: username
+            });
+            return response
+        } catch (error) {
+            console.error('Error while logging in: ', error);
+            return
+        }
+    }
+    async setInfo(user, data){
+        try{
+            const response = await axios.post(`${API_URL}/info_person/`, {
+                username: data.username,
+                firstName : data.firstName,
+                lastName : data.lastName,
+                sex: data.sex,
+                birthDate: data.birthDate,
+                bloodType: data.bloodType,
+                organDonor: data.organDonor,
+                weight: data.weight,
+                height: data.height,
+                maritalStatus : data.maritalStatus,
+                color : data.color,
+                nationality : data.nationality,
+                occupation : data.occupation,
+                address : data.address,
+                phone : data.phone,
+                primaryLanguague: data.primaryLanguague
+            });
+            return response
+        } catch (error) {
+            console.error('Error while setting profile info: ', error);
+            return
+        }
+    }
+    async updateInfo(user, data){
+        try{
+            const response = await axios.put(`${API_URL}/info_person/`, {
+                username: data.username,
+                firstName : data.firstName,
+                lastName : data.lastName,
+                sex: data.sex,
+                birthDate: data.birthDate,
+                bloodType: data.bloodType,
+                organDonor: data.organDonor,
+                weight: data.weight,
+                height: data.height,
+                maritalStatus : data.maritalStatus,
+                color : data.color,
+                nationality : data.nationality,
+                occupation : data.occupation,
+                address : data.address,
+                phone : data.phone,
+                primaryLanguague: data.primaryLanguague
+            });
+            return response
+        } catch (error) {
+            console.error('Error while setting profile info: ', error);
+            return
+        }
+    }
+}
+
+export default new ProfileTabService()
