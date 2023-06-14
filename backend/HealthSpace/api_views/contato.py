c4ecdc218cf141d273c489ae1501af108fb8a08e
GScrok <grmscrok@gmail.com>
2023-06-14 11:41:51 -0300

    .

---
+++ backend/HealthSpace/api_views/contato.py
@@ -0,0 +1,28 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from HealthSpace.models import tbEmergencyContact
+from HealthSpace.serializer import EmergencyContactSerializer
+
+
+class ContatoEmergencia(APIView):
+    def get(self, request, username):
+        retorno_dados = []
+        tabela_contato = tbEmergencyContact.objects.filter(username=username)
+        for cada_contato_emergencia in tabela_contato:
+            retorno_dados.append({
+                'name':cada_contato_emergencia.name,
+                'relationship': cada_contato_emergencia.relationship,
+                'phone':cada_contato_emergencia.phone,
+            })
+            return Response(retorno_dados, status=200)
+
+    def post(self, request):
+        data = request.data
+        serializer = EmergencyContactSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer._errors, status=400)
+
+        return Response(status=201)
+    
